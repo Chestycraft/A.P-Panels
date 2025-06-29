1cea590905a957dc2f0d36f499c47ac851a91078
Brent <brentixorgeix@gmail.com>
2025-06-30 04:40:49 +0800

    Install @tailwindcss/vite for Tailwind v4

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcssVite from '@tailwindcss/vite'
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
     base: './', // for relative paths (especially for GitHub Pages or Vercel)
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcssVite()],
   resolve:{
     alias:{
     "@":path.resolve(__dirname,"./src")
